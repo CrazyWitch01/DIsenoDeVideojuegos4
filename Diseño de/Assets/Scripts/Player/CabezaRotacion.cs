c4f6212ca07cea920f0e2da93e36767dff909e48
CrazyWitch01 <ignaciopenasumari@gmail.com>
2025-07-02 16:06:09 -0500

    ControlesGamepadAñadidos

--- Diseño de/Assets/Scripts/Player/CabezaRotacion.cs
+++ Diseño de/Assets/Scripts/Player/CabezaRotacion.cs
@@ -4,21 +4,50 @@ using UnityEngine.InputSystem;
 
 public class CabezaRotacion : MonoBehaviour
 {
+    private InputSystem_Actions input;
     private Camera cam;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Sensibilidad del stick derecho")]
+    public float stickDeadZone = 0.2f;
+
+    void Awake()
+    {
+        input = new InputSystem_Actions();
+    }
+
+    void OnEnable()
+    {
+        input.NewPlayer.Enable();
+    }
+
+    void OnDisable()
+    {
+        input.NewPlayer.Disable();
+    }
+
     void Start()
     {
         cam = Camera.main;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 mousePantalla = Mouse.current.position.ReadValue();
-        Vector3 mouseMundo = cam.ScreenToWorldPoint(mousePantalla);
-        Vector2 direction = (Vector2)(mouseMundo - transform.position);
+        Vector2 stickInput = input.NewPlayer.Look.ReadValue<Vector2>();
 
-        float anguloz = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0, 0, anguloz - 90f);
+        if (stickInput.magnitude > stickDeadZone)
+        {
+            float anguloZ = Mathf.Atan2(stickInput.y, stickInput.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, anguloZ - 90f);
+        }
+        else
+        {
+            Vector2 mousePantalla = Mouse.current.position.ReadValue();
+            Vector3 mouseMundo = cam.ScreenToWorldPoint(mousePantalla);
+            Vector2 direccion = (Vector2)(mouseMundo - transform.position);
+
+            float anguloZ = Mathf.Atan2(direccion.y, direccion.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, 0, anguloZ - 90f);
+        }
     }
 }
+ 
